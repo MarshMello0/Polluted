396f37a96809d74de1674998a2d358c638d179f6
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-21 22:51:33 +0100

    Selection Button Feedback

--- Assets/Scripts/Utilities/TMPButton.cs
+++ Assets/Scripts/Utilities/TMPButton.cs
@@ -1,12 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Security.Policy;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
+using System;
 
-public class TMPButton : MonoBehaviour, IPointerClickHandler
+public class TMPButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public EventTrigger.TriggerEvent callBack;
+    private TextMeshProUGUI text;
+    
+    private Color selectedColor = Color.yellow;
+    private Color defaultColor;
+    private Color clickedColor = Color.cyan;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        defaultColor = text.color;
+    }
+
+    private IEnumerator SelectColorDelay()
+    {
+        text.color = clickedColor;
+        yield return new WaitForSeconds(1);
+        text.color = defaultColor;
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        StartCoroutine(SelectColorDelay());
         callBack.Invoke(eventData);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = selectedColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = defaultColor;
     }
 }
